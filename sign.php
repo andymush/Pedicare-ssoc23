bd14d93d60274a07d40aac19d935f5aa4ddc3241
andymush <andymush9@gmail.com>
2023-05-15 10:46:42 +0300

    changed a few items for file referencing

--- sign.php
+++ sign.php
@@ -8,6 +8,7 @@ include("connect.php");
     <meta charset="UTF-8">
     <title> Login and Registration Form in HTML & CSS | CodingLab </title>
     <link rel="stylesheet" href="sign.css">
+    <link rel="shortcut icon" type="image/jpg" href="images/logo.png"/>
     <!-- Fontawesome CDN Link -->
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
